8ea74f4189a0f84144ad9fa7b56c23b0bc934fe7
heartlessboopa <boopathimanikandan1234@gmail.com>
2023-09-25 23:24:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function cls(){
+    document.getElementById("result").value="";
+    }
+
+function display(document.getElementById("result")){
+    document.getElementById("result").value="";
+     }
