a261d13b9bdde7c5e93a1550f109707d1b57d18b
heartlessboopa <boopathimanikandan1234@gmail.com>
2023-09-25 23:29:29 +0530

    JS UPDATED!!!!!!

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 function cls(){
     document.getElementById("result").value="";
     }
-
-function display(document.getElementById("result")){
-    document.getElementById("result").value="";
-     }
